03683dc45278395661f9264a3fb47a81f4291839
fluistro <sam674526@gmail.com>
2024-07-16 14:30:35 -0700

    Updated home component with authentication template

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 
 export default function Home() {
-    return (
+
+    // Use api endpoint to get session
+    const user_id = "example-id";
+
+    return user_id ? (
         <div>
             <h1>Homepage</h1>
             <Outlet />
         </div>
-    )
+    ) : <p>Please <a href="/login">log in</a></p>;
 };
